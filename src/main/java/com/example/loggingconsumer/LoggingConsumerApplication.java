6306535e946219ef7d585408324976716272f917
Leipo Yan <leipo.yan@solace.com>
2020-09-20 20:57:25 +0800

    Add Supplier and Consumer and SSL trust store.

--- src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
+++ src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
@@ -1,6 +1,8 @@
 package com.example.loggingconsumer;
 
 import java.util.function.Function;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,11 +15,23 @@ public class LoggingConsumerApplication {
 		SpringApplication.run(LoggingConsumerApplication.class, args);
 	}
 
+	/*
     @Bean
 	public Function<String, String> uppercase() {
 	    return value -> {
 	        System.out.println("Received: " + value);
 	        return value.toUpperCase();
 	    };
+	}
+    */
+	
+	@Bean
+	public Supplier<String> source() {
+		return () -> "Solace SCS test message!";
+	}
+	
+	@Bean
+	public Consumer<String> sink() {
+		return System.out::println;
 	}
 }
