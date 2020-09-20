5eee10fc0265ff8905cccc25f6e3faca68cc6193
Leipo Yan <leipo.yan@solace.com>
2020-09-20 18:27:00 +0800

    Use Inherited Configuration Options for broker configuration

--- src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
+++ src/main/java/com/example/loggingconsumer/LoggingConsumerApplication.java
@@ -1,7 +1,10 @@
 package com.example.loggingconsumer;
 
+import java.util.function.Consumer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoggingConsumerApplication {
@@ -10,4 +13,23 @@ public class LoggingConsumerApplication {
 		SpringApplication.run(LoggingConsumerApplication.class, args);
 	}
 
+	@Bean
+	public Consumer<Person> log() {
+	    return person -> {
+	        System.out.println("Received: " + person);
+	    };
+	}
+
+	public static class Person {
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String toString() {
+			return this.name;
+		}
+	}
 }
